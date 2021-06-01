91df33991b00d9e4103adb2291379cac1c849628
Elli Raynai <elli@elliraynai.com>
2021-06-01 09:15:02 -0400

    created ui for game hooked up one button

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 /*--CONSTANTS--*/
 
-const gamePatternOne = ["green", "green", "yellow", "green", "yellow", "green"];
+const gamePattern = ["green", "green", "yellow", "green", "yellow", "blue", "green", "yellow", "blue", "red"];
 
 
 /*--APP'S STATE VARIABLES--*/
 
 let score = 0;
-
 let userPattern = [];
 let turn = 0;
 
@@ -17,6 +16,8 @@ let scoreEl = document.getElementById("score-number");
 let startEl = document.getElementById("start");
 let greenButtonEl = document.getElementById("green");
 let yellowButtonEl = document.getElementById("yellow");
+let blueButtonEl = document.getElementById("blue");
+let redButtonEl = document.getElementById("red");
 let playerMessageEl = document.getElementById('playerMessage');
 
 /*--EVENT LISTENERS--*/
@@ -29,13 +30,15 @@ yellowButtonEl.addEventListener("click", buttonIsClicked);
 
 function buttonIsClicked(e) {
     if(e.target.id === startEl.id){
+        console.log("are you starting");
         patternDriver();
     }else{
         userPattern.push(e.target.id);
+        console.log(userPattern);
         function checkPattern(){
             for(let i = 0; i < userPattern.length; i++){
-                if(userPattern[i] !== gamePatternOne[i]){
-                console.log("this is false");
+                if(userPattern[i] !== gamePattern[i]){
+                console.log('are you failing here?');
                 return false;
                 }    
             }
@@ -43,18 +46,19 @@ function buttonIsClicked(e) {
         }
         if(checkPattern()){
             turn++;
+            console.log(turn);
             patternDriver();
-            checkWinner(); 
+           
         }else{
             failMessage();
             resetGame();
-    
         } 
     }
 }      
 
 function patternDriver(){
     if(turn === 0){
+        addGreenBorder();
         return lightFirstPattern();
     }
     if(turn === 1) {
@@ -65,23 +69,43 @@ function patternDriver(){
     }   
 }
 
+
+
 function checkWinner(){ 
-    if(turn >= gamePatternOne.length){
-        console.log('player has won!');
+    if(turn >= gamePattern.length){
+        playerMessageEl.textContent = "Player has beaten Simon!";
     }
 }
 
+/* function patternDriver(){
+    for(let color of gamePattern){
+        if(color === "green"){
+            setTimeout(addGreenBorder, 300);
+            setTimeout(removeGreenBorder, 600);
+        }else if(color === "yellow"){
+            setTimeout(addYellowBorder, 500);
+            setTimeout(removeYellowBorder, 900);
+        }else if(color === "blue"){
+            setTimeout(addBlueBorder, 800);
+            setTimeout(removeBlueBorder, 1200);
+        }else if(color === "red"){
+            setTimeout(addRedBorder, 1100);
+            setTimeout(removeRedBorder, 1500);
+        }
+    }
+} */
+
 function lightFirstPattern(){
-    setTimeout(addGreenBorder, 300);
-    setTimeout(removeGreenBorder, 900);
+    /* setInterval(addGreenBorder, 300); */
+    setTimeout(removeGreenBorder, 2000);
     
 }
 
 function lightSecondPattern(){
     setTimeout(addGreenBorder, 1000);
-    setTimeout(removeGreenBorder, 1600);
+    setTimeout(removeGreenBorder, 2600);
     setTimeout(addYellowBorder, 1700);
-    setTimeout(removeYellowBorder, 2300);
+    setTimeout(removeYellowBorder, 3300);
 }
 
 function lightThirdPattern(){
@@ -89,32 +113,50 @@ function lightThirdPattern(){
     setTimeout(removeGreenBorder, 3000);
     setTimeout(addYellowBorder, 3100);
     setTimeout(removeYellowBorder, 3700);
-    setTimeout(addGreenBorder, 3800);
-    setTimeout(removeGreenBorder, 4400);
+    setTimeout(addBlueBorder, 3800);
+    setTimeout(removeBlueBorder, 4400);
 }
 
 
+
+
 function render(){
-    
+    checkWinner(); 
 }
 
 setInterval(render, 300);
 
 
 function addGreenBorder(){
-    greenButtonEl.classList.add("addGreenBorder");
+    greenButtonEl.classList.add("green-ani");
 }
 
 function addYellowBorder(){
-    yellowButtonEl.classList.add("addYellowBorder");
+    yellowButtonEl.classList.add("yellow-ani");
+}
+
+function addBlueBorder(){
+    blueButtonEl.classList.add("addBlueBorder");
+}
+
+function addRedBorder(){
+    redButtonEl.classList.add("addRedBorder");
 }
 
  function removeGreenBorder(){
-    greenButtonEl.classList.remove("addGreenBorder");
+    greenButtonEl.classList.remove("green-ani");
 }
 
 function removeYellowBorder(){
-    yellowButtonEl.classList.remove("addYellowBorder");
+    yellowButtonEl.classList.remove("yellow-ani");
+}
+
+function removeBlueBorder(){
+    blueButtonEl.classList.remove("addBlueBorder");
+}
+
+function removeRedBorder(){
+    redButtonEl.classList.remove("addRedBorder");
 }
 
 function failMessage(){
