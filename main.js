a7adfc990e9390e685fa88a1f5829bb9de4ef4fc
Elli Raynai <elli@elliraynai.com>
2021-06-05 19:30:01 -0400

    working out logic and creating functions for lighting up patterns

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 /*--CONSTANTS--*/
-const pattern = ["green", "yellow", "blue", "red"];
+/* const pattern = ["green", "yellow", "blue", "red"];
 const pattern2 = ["green", "yellow", "blue", "red", "orange"];
 const getItemOne = new Audio("get-item-1-8-bit.mp3");
 const getItemTwo = new Audio("get-item-2-8-bit.mp3");
 const getItemThree = new Audio("get-item-3-8-bit.mp3");
 const getItemFour = new Audio("get-item-4-8-bit.mp3");
 const errorSound = new Audio("error-8-bit-2.mp3");
-const winSound = new Audio("jump-spring-8-bit.mp3");
+const winSound = new Audio("jump-spring-8-bit.mp3"); */
 
 
 /*--APP'S STATE VARIABLES--*/
 
-let score = 0;
+/* let score = 0;
 let generatedSequence = [];
 let userPattern = [];
 let turn = 0;
 let easyTimer =   [2000, 3000, 4000, 5000, 6000];
 let mediumTimer = [1000, 2000, 3000, 4000, 5000];
 let hardTimer =   [500, 1000, 2000, 3000, 4000];
-let finalTimer = [];
+let finalTimer = []; */
 
 /*--CACHED ELEMENT REFERENCES--*/
 
-let scoreEl = document.getElementById("score-number");
+/* let scoreEl = document.getElementById("score-number");
 let startEl = document.getElementById("start");
 let greenButtonEl = document.getElementById("green");
 let yellowButtonEl = document.getElementById("yellow");
@@ -35,17 +35,17 @@ let easyB = document.getElementById("easyB");
 let mediumB = document.getElementById("mediumB");
 let hardB = document.getElementById("hardB");
 let restartB = document.getElementById("restartB");
-
+ */
 /*--EVENT LISTENERS--*/
 
-easyB.addEventListener("click", buttonIsClicked);
+/* easyB.addEventListener("click", buttonIsClicked);
 mediumB.addEventListener("click", buttonIsClicked);
 hardB.addEventListener("click", buttonIsClicked);
-restartB.addEventListener("click", buttonIsClicked);
+restartB.addEventListener("click", buttonIsClicked); */
 
 
 
-function buttonIsClicked(e) {
+/* function buttonIsClicked(e) {
     buttonPattern(e);
     restartButtonPattern(e);
 }
@@ -56,32 +56,13 @@ function addEventListeners(){
     blueButtonEl.addEventListener("click", buttonIsClicked);
     redButtonEl.addEventListener("click", buttonIsClicked); 
 }
+ */
 
-function generateNextSequence(){
-    //get a random number from the length of the easy pattern array
-    //save random number inside a variable chosen color
-    //push the color inside the generated sequence array
-} 
 
-function animateSequence(){
-    //loop through the generated sequence array and pass into boardmaker(flash color) function 
-}
 
-fuction getPatterns(){
-    counter = 0;
-    //check the 
-    //listen to the user click
-    //get the event and cross check it with generateNextSequence
-    //if not right do a fail message
-    //if correct, increment counter++
-    //if its greater than the geneatedSequences we loop back to 1
 
-    //while checkingSequence 
-}
-
-function buttonPattern(el){
+/* function buttonPattern(el){
     if(el.target.id === easyB.id || el.target.id === mediumB.id || el.target.id === hardB.id){
-        /* level set up for difficulty */
         switch(el.target.id){
             case easyB.id:
                 finalTimer = easyTimer;
@@ -240,3 +221,84 @@ function render(){
 
 setInterval(render, 100);
 
+ */
+
+/* 
+When the user clicks on the easy button, the computer generates a random color and blinks that random color.  The computer has a counter which it uses to determine how many sequences it shows to the user.
+This begins the game loop where the button that the user clicks on is captured by the computer by inserting it into an array and then comparing what is currently in the array that it generated before. If the user clicks on the right button, the counter is increased, the array is emptied, and then the counter number is used to determine how many colors are pushed into the next selection.  It then starts blinking that number of selections as the loop starts all over again
+
+
+ \
+ which button the user and inserts that color in an empty array.  It then compares what the user clicked to what is inside the 
+
+*/
+
+/*--CONSTANTS--*/
+
+const pattern = ["green", "yellow", "blue", "red"];
+
+/*--APP'S STATE VARIABLES--*/
+
+let generatedSequence = [];
+let sequenceCounter = 1;
+let firstTimer = 0;
+let secondTimer = 1000;
+
+/*--CACHE ELEMENT REFERENCES--*/
+
+/*--EVENT LISTENERS--*/
+
+/*--FUNCTIONS--*/
+
+
+generateNextSequence();
+
+function generateNextSequence(){
+    for(let i = 1; i <= sequenceCounter; i++){
+        let randomNumber = Math.floor(Math.random() * pattern.length);
+        generatedSequence.push(pattern[randomNumber]);
+    }
+} 
+
+console.log(generatedSequence);
+
+function animateSequence(){
+    for(let i = 0; i < generatedSequence.length; i++){
+        lightUpPatterns(generatedSequence[i]);
+    }
+}
+
+//run this function after I click
+function timerIncrease(){
+    firstTimer += 1000;
+    secondTimer += 1000;
+}
+
+function lightUpPatterns(color){
+    if(color === "green"){
+        setTimeout(addGreenBorder, firstTimer);
+        setTimeout(removeGreenBorder, secondTimer);
+    }else if(color === "yellow"){
+        setTimeout(addYellowBorder, firstTimer);
+        setTimeout(removeYellowBorder, secondTimer);
+    }else if(color === "blue"){
+        setTimeout(addBlueBorder, firstTimer);
+        setTimeout(removeBlueBorder, secondTimer);
+    }else if(color === "red"){
+        setTimeout(addRedBorder, firstTimer);
+        setTimeout(removeRedBorder, secondTimer);
+    }
+}
+
+
+function getPatterns(){
+    counter = 0;
+    //check the 
+    //listen to the user click
+    //get the event and cross check it with generateNextSequence
+    //if not right do a fail message
+    //if correct, increment counter++
+    //if its greater than the geneatedSequences we loop back to 1
+
+    //while checkingSequence 
+}
